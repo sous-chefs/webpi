364d3764528211a3376fc21da9c395cc1cf68d9d
Tim Smith <tsmith84@gmail.com>
2015-10-21 16:06:56 -0700

    Convergence tests on multiple windows platforms

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,10 +1,38 @@
 require 'spec_helper'
 
-describe 'default recipe on Ubuntu 14.04' do
+describe 'default recipe on Windows 2008' do
   let(:chef_run) do
-    ChefSpec::ServerRunner.new do |node|
-      node.automatic[:lsb][:codename] = 'trusty'
-    end.converge('webpi::default')
+    ChefSpec::ServerRunner.new(platform: 'windows', version: '2008').converge('webpi::default')
+  end
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+end
+
+describe 'default recipe on Windows 2008 R2' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new(platform: 'windows', version: '2008R2').converge('webpi::default')
+  end
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+end
+
+describe 'default recipe on Windows 2012' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new(platform: 'windows', version: '2012').converge('webpi::default')
+  end
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+end
+
+describe 'default recipe on Windows 2012 R2' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new(platform: 'windows', version: '2012R2').converge('webpi::default')
   end
 
   it 'converges successfully' do
