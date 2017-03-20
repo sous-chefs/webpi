c6dbf1e4846c7875a568032ca3ab08e7edc00102
Tim Smith <tsmith@chef.io>
2017-03-20 09:37:41 -0700

    Test in appveyer and require windows 2.0+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name             'webpi'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures the Microsoft Web Platform Installer (WebPI)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '4.0.0'
 supports         'windows'
-depends          'windows', '>= 1.39.0'
+depends          'windows', '>= 2.0'
 
 source_url 'https://github.com/chef-cookbooks/webpi'
 issues_url 'https://github.com/chef-cookbooks/webpi/issues'
