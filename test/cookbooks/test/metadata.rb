b7277988b11999aaae84c5c021f81dbd37aa1cd2
Tim Smith <tsmith@chef.io>
2017-04-05 22:47:21 -0700

    Add a test recipe that actually test the resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ test/cookbooks/test/metadata.rb
@@ -0,0 +1,6 @@
+name 'test'
+license 'Apache-2.0'
+description 'Test cookbook for WebPI'
+version '1.0.0'
+
+depends 'webpi'
---
+++ test/cookbooks/test/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook:: test
+# Recipe:: default
+#
+# Copyright:: 2017, Chef Software, Inc.
+
+include_recipe 'webpi::default'
+
+webpi_product 'PHP54' do
+  accept_eula true
+  action :install
+end
