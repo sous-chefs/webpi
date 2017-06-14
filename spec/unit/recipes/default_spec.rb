5498863de5eb8f71ea143b70410e6d6de80aa26b
Tim Smith <tsmith@chef.io>
2017-06-14 16:59:30 -0700

    Remove windows 2008 chefspec
    We support 2008 R2+
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
-describe 'default recipe on Windows 2008' do
-  let(:chef_run) do
-    ChefSpec::ServerRunner.new(platform: 'windows', version: '2008').converge('webpi::default')
-  end
-
-  it 'converges successfully' do
-    expect { :chef_run }.to_not raise_error
-  end
-end
-
 describe 'default recipe on Windows 2008 R2' do
   let(:chef_run) do
     ChefSpec::ServerRunner.new(platform: 'windows', version: '2008R2').converge('webpi::default')
