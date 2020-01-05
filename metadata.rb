02356dbb98fe34a0768332b3707cfbba760e5b48
Tim Smith <tsmith84@gmail.com>
2020-01-05 11:51:58 -0800

    Require Chef 12.15+
    This gives us modern metadata parsing
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -9,4 +9,4 @@ depends          'windows', '>= 2.0'
 
 source_url 'https://github.com/chef-cookbooks/webpi'
 issues_url 'https://github.com/chef-cookbooks/webpi/issues'
-chef_version '>= 12.6'
+chef_version '>= 12.15'
