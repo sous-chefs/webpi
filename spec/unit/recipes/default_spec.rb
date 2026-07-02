89c3f1d54056a04f3a04669e7db43bf5cdf9c3f2
Dan Webb <dan.webb@damacus.io>
2026-07-02 02:46:11 +0100

    fix: notify archive extraction for zip install

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -15,3 +15,15 @@ describe 'default recipe on Windows 2016' do
     expect { :chef_run }.to_not raise_error
   end
 end
+
+describe 'webpi::install-zip' do
+  platform 'windows', '2016' do |node|
+    node.override['webpi']['install_method'] = 'zip'
+  end
+
+  let(:zip_path) { "#{Chef::Config[:file_cache_path]}/WebPICMD.zip" }
+
+  it 'extracts the downloaded WebPI command line archive' do
+    expect(chef_run.remote_file(zip_path)).to notify("archive_file[#{zip_path}]").to(:extract).immediately
+  end
+end
